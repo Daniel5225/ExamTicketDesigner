7b78376a1f89e38274780f9f0edd712c1ac5f628
Daniel <Daniel5225@users.noreply.github.com>
2020-03-22 20:54:13 +0300

    Preparation for the implementation of the functional part of the program

--- HomeWindow.xaml.cs
+++ HomeWindow.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System.Diagnostics;
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
 using System.Windows;
+using System.IO;
+
 
 namespace ExamTicketDesigner
 {
@@ -8,6 +12,8 @@ namespace ExamTicketDesigner
     /// </summary>
     public partial class HomeWindow : Window
     {
+        string pathtofile = "";
+        string[] questions;
         public HomeWindow()
         {
             InitializeComponent();
@@ -22,6 +28,38 @@ namespace ExamTicketDesigner
         {
             System.Diagnostics.Process proc = System.Diagnostics.Process.Start("Help\\news.exe"); //Запускаем скрипт парсинга новостей
             proc.WaitForExit();//и ждем, когда он завершит свою работу 
+        }
+
+        private void PathButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Multiselect = false;
+            fileDialog.Filter = "Текстовые файлы|*.txt";
+            fileDialog.DefaultExt = ".txt";
+            Nullable<bool> dialogOK = fileDialog.ShowDialog();
+
+            if (dialogOK == true)
+            {
+                pathtofile = fileDialog.FileName.Trim();
+            }
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            int qnumber = Convert.ToInt16(Qnumber1.Text) + Convert.ToInt16(QNumber2.Text);
+            if (qnumber >= 4 || qnumber <=0)
+            {
+                MessageBox.Show("Максимальное количество вопросов - 3");
+            }
+            else
+            {                
+                questions = File.ReadAllLines(@"" + pathtofile + "", System.Text.Encoding.Default);
+            }
+        }
+
+        private void ScoreDefaultCB_Checked(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
